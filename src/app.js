e2ec27e991da6f61b14b90c723a80be34d87aa4e
siddharth787898 <siddharthkaseera986@gmail.com>
2025-08-23 16:48:38 +0530

    playing with routs

--- src/app.js
+++ src/app.js
@@ -23,22 +23,55 @@ const app = express();
 
 //this method will only match, get api calls to the rout /user
 
+// app.get("/user",(req,res)=>{
+//   res.send({firstname :"Siddharth", lastname:"Kasera"})
+// })
+
+// app.post("/user",(req,res)=>{
+//   res.send("data is successfually send");
+// })
+
+// app.delete("/user",(req,res)=>{
+// res.send("data deleted");
+// })
+
+
+// app.use("/test",(req,res)=>{
+//   res.send("tessssting");
+// })
+
+// app.get ("/a/",(req, res)=>{
+//   res.send({Firstname:"Sidd",Lastname:"kas"});
+// })
+
+//this is how you read the querry perameters
+//in postman http://localhost:3000/user?userid=101&technic= mart 
+//& this is using for adding more
+app.get ("/user",(req, res)=>{
+  console.log(req.query)
+  res.send({Firstname:"Sidd",Lastname:"kas"});
+})
+
+// now how to handel this 
+//how to get the user id 
+//user perams to get the user id 
+//http://localhost:3000/user/707 if you hit the send so you get the userid 
+// app.get("/user/:userid",(req,res)=>{
+//   console.log(req.params );
+//   res.send({FN:"shin",LM:"kuma"})
+// })
+
+//http://localhost:3000/user/707/Shin
+app.get("/user/:userid/:name/",(req,res)=>{
+  console.log(req.params );
+  res.send({FN:"shin",LM:"kuma"})
+})
+
 app.get("/user",(req,res)=>{
-  res.send({firstname :"Siddharth", lastname:"Kasera"})
+  console.log(query)
+  res.send({FN:"shin",LM:"kuma"})
 })
 
-app.post("/user",(req,res)=>{
-  res.send("data is successfually send");
-})
-
-app.delete("/user",(req,res)=>{
-res.send("data deleted");
-})
-
-
-app.use("/test",(req,res)=>{
-  res.send("tessssting");
-})
 
 app.listen(3000,()=>{
   console.log("here is the serve");
